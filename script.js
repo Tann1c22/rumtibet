27cd2327d939a618b6b87d2bd1d009926fa6119d
Tann1c22 <iborovkov804@gmail.com>
2024-12-27 20:19:22 +0400

    added adapted about and program sections

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 import "./css/style.sass";
+const form = document.querySelector(".subscribe__form");
+const input = form.querySelector(".subscribe__input");
+
+
+function formSubmit(e){
+    let inputValue = input.value
+
+    e.preventDefault();
+    console.log("Submit form");
+    console.log(inputValue);
+    
+}
+form.addEventListener("submit", formSubmit);
